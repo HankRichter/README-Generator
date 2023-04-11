68497dae053638a95ec8546539ce392f4feca456
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-04-10 22:57:30 -0400

    Added questions array

---
+++ Main/index.js
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Title of the project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description of the project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation steps for the project.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How can the project be used?",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Choose the license you would like to assign to the project.",
+    choices: ["MIT", "GPL", "CC", "No License"],
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contributors to this project.",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Test instructions for the project.",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email.",
+  },
+];
+
