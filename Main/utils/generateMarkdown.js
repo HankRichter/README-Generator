a98890ac33bfd4155e207c70ab30644c637b1106
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-04-10 22:59:21 -0400

    Wrote the generate Markdown function

---
+++ Main/utils/generateMarkdown.js
@@ -0,0 +1,32 @@
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## License:
+  ${renderLicenseBadge(data.license[0])}
+
+  ${renderLicenseSection(data.license[0])}
+  ${renderLicenseLink(data.license[0])}
+  ## Table of Contents:
+  ### -[License](#license)
+  ### -[Description](#description)
+  ### -[Installation](#instllation)
+  ### -[Usage](#usage)
+  ### -[Contribution](#contribution)
+  ### -[Testing](#testing)
+  ### -[Questions](#questions)
+
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## Contribution:
+  ${data.contribution}
+  ## Testing:
+  ${data.test}
+  ## Questions:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: [${data.email}](mailto:user@example.com)
+`;
+}
+
